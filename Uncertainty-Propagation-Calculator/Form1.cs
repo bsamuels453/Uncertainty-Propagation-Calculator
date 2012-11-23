7daadd710df8b082f402775672b1f86cb1ea4158
Ben Samuels <bsamuels453@gmail.com>
2012-11-23 15:40:13 -0800

    implementing IsVariableTableValid

--- Uncertainty-Propagation-Calculator/Form1.cs
+++ Uncertainty-Propagation-Calculator/Form1.cs
@@ -14,6 +14,7 @@ using PdfToImage;
 namespace Uncertainty_Propagation_Calculator{
     public partial class Form1 : Form{
         readonly LatexToImg _latexConverter;
+        readonly string[] _symbolBlacklist= new []{"+", "*", "-", "/", "^", ".", "ln", "log", "e", "(", ")"};
 
         public Form1(){
             InitializeComponent();
@@ -32,6 +33,7 @@ namespace Uncertainty_Propagation_Calculator{
             _latexConverter.QueueNewUpdate(" ");
         }
 
+        #region handle ui stuff
         void GetKeyButClick(object sender, EventArgs e){
             const string target1 = "https://developer.wolframalpha.com/portal/apisignup.html";
             try{
@@ -52,7 +54,7 @@ namespace Uncertainty_Propagation_Calculator{
             prc.Start();
         }
 
-        private void SaveKeyButClick(object sender, EventArgs e) {
+        void SaveKeyButClick(object sender, EventArgs e) {
             string key = WolframApiTextBox.Text;
             var sw = new StreamWriter("apikey.txt", false);
             sw.Write(key);
@@ -61,13 +63,105 @@ namespace Uncertainty_Propagation_Calculator{
         }
 
         void PdfConversionCallback(){
-            //FormulaRenderWindow.Refresh();
             pictureBox1.ImageLocation = Directory.GetCurrentDirectory() + "\\LeanAndMeanPdfLatex\\bin\\win32\\formula.jpg";
         }
 
-        private void Button1Click(object sender, EventArgs e) {
+        void Button1Click(object sender, EventArgs e) {
             _latexConverter.QueueNewUpdate(EquationEntryTextBox.Text);
         }
+
+        void CalculateButClick(object sender, EventArgs e) {
+            string s;
+            if (!IsVariableTableValid(out s)) {
+                DataInputErrLabel.Text = s;
+                DataInputErrLabel.Visible = true;
+            }
+            else{
+                DataInputErrLabel.Visible = false;
+            }
+        }
+        #endregion
+
+        bool IsVariableTableValid(out string retText){
+            var variableNames = new List<string>();
+            for (int i = 0; i < VariableEntryGrid.RowCount; i++){
+
+                //check for empty cells
+                //skip empty rows
+                if (VariableEntryGrid[0, i].Value == null && VariableEntryGrid[1, i].Value == null && VariableEntryGrid[2, i].Value == null) {
+                    continue;
+                }
+
+                //if at least one column has text in it in the row and the others dont, then incomplete
+                if (VariableEntryGrid[0, i].Value != null || VariableEntryGrid[1, i].Value != null || VariableEntryGrid[2, i].Value != null){
+                    if (VariableEntryGrid[0, i].Value == null || VariableEntryGrid[1, i].Value == null || VariableEntryGrid[2, i].Value == null){
+                        retText = "ERROR: Row " + i + " Variable data incomplete";
+                        return false;
+                    }
+                }
+
+                string variableName = (string)VariableEntryGrid[0, i].Value;
+                variableNames.Add(variableName);
+
+                foreach (var s in _symbolBlacklist){
+                    if (variableName.Contains(s)){
+                        retText = "ERROR: Row " + i + "'s variable name contains invalid character(s) '" + s + "'";
+                        return false;
+                    }
+                    foreach (var chr in variableName){
+                        if (char.IsDigit(chr)){
+                            retText = "ERROR: Row " + i + "'s variable name contains a number (not allowed)";
+                            return false;
+                        }
+                    }
+                }
+
+                double _;
+                //first value column
+                string sciNotFix = (string)(VariableEntryGrid[1, i].Value);
+                sciNotFix = sciNotFix.Replace("*10^", "E");
+                if(!double.TryParse(sciNotFix, out _)){
+                    retText = "ERROR: Row " + i + "'s variable value is invalid, it should be a number";
+                    return false;
+                }
+
+                //second value column
+                sciNotFix = (string)(VariableEntryGrid[2, i].Value);
+                sciNotFix = sciNotFix.Replace("*10^", "E");
+                if (!double.TryParse(sciNotFix, out _)) {
+                    retText = "ERROR: Row " + i + "'s uncertainty value is invalid, it should be a number";
+                    return false;
+                }
+            }
+
+            //now make sure none of the variable names contain substrings of other variable names
+            foreach (var varName in variableNames) {
+                foreach (var varNameToSub in variableNames) {
+                    if (varName.Contains(varNameToSub) && varName != varNameToSub) {
+                        retText = "ERROR: Variable " + varName + " contains an substring identical to variable " + varNameToSub;
+                        return false;
+                    }
+                }
+            }
+            retText = "";
+            return true;
+        }
+
+
+        /*
+        string equation = EquationEntryTextBox.Text;
+
+        //split equation into two sides
+        var sides = equation.Split('=');
+        var leftSide = sides[0] + "=";
+        var rightSide = sides[1];
+
+        var symbolAliases = ExpressionManip.GenerateEquationAliases(rightSide);
+
+        string aliasEquation = ExpressionManip.ConvertSymbolsIntoAliases(symbolAliases, rightSide);
+
+        var solver = new WolframEvaluator(WolframApiTextBox.Text);
+        */
 
     }
 }
