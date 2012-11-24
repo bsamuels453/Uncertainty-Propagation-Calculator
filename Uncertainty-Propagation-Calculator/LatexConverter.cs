5f4bb8235a4a32ef0c701778c49b6033d2f142b9
Ben Samuels <bsamuels453@gmail.com>
2012-11-23 18:21:23 -0800

    rudimentary latex conversion implemented

--- Uncertainty-Propagation-Calculator/LatexConverter.cs
+++ Uncertainty-Propagation-Calculator/LatexConverter.cs
@@ -1,4 +1,20 @@
-﻿namespace Uncertainty_Propagation_Calculator{
+﻿using System;
+
+namespace Uncertainty_Propagation_Calculator{
     internal class LatexConverter{
+
+
+        public static string ConvertExpressionToLatex(string expression){
+            //  / greek
+            int pos;
+            while ((pos = expression.IndexOf('/')) != -1) {
+                var right = ExpressionManip.SplitEquation(expression, pos, ExpressionManip.SplitType.RightSide);
+                var left = ExpressionManip.SplitEquation(expression, pos, ExpressionManip.SplitType.LeftSide);
+
+                expression = expression.Remove(left.StartIndex, right.EndIndex - left.StartIndex);
+                expression = expression.Insert(left.StartIndex, "\\frac{" + left.Segment + "}{" + right.Segment + "}");
+            }
+            return expression;
+        }
     }
 }
