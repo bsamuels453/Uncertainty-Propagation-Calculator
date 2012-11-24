055ace4551cb0fc5e5c88ecfed2d628a4d43a4f6
Ben Samuels <bsamuels453@gmail.com>
2012-11-23 18:46:51 -0800

    added symbol conversion to latex converter

--- Uncertainty-Propagation-Calculator/LatexConverter.cs
+++ Uncertainty-Propagation-Calculator/LatexConverter.cs
@@ -5,7 +5,7 @@ namespace Uncertainty_Propagation_Calculator{
 
 
         public static string ToLatex(string expression){
-            //  / greek
+            //convert fractions
             int pos;
             while ((pos = expression.IndexOf('/')) != -1) {
                 var right = ExpressionManip.SplitEquation(expression, pos, ExpressionManip.SplitType.RightSide);
@@ -14,6 +14,32 @@ namespace Uncertainty_Propagation_Calculator{
                 expression = expression.Remove(left.StartIndex, right.EndIndex - left.StartIndex);
                 expression = expression.Insert(left.StartIndex, "\\frac{" + left.Segment + "}{" + right.Segment + "}");
             }
+
+            //convert math symbols
+            expression = expression.Replace("ρ", @"\rho");
+            expression = expression.Replace("π", @"\pi");
+            expression = expression.Replace("Ω", @"\Omega");
+            expression = expression.Replace("δ", @"\delta");
+            expression = expression.Replace("Δ", @"\Delta");
+            expression = expression.Replace("α", @"\alpha");
+            expression = expression.Replace("μ", @"\mu");
+            expression = expression.Replace("ε", @"\epsilon");
+            expression = expression.Replace("θ", @"\theta");
+            expression = expression.Replace("κ", @"\kappa");
+            expression = expression.Replace("σ", @"\sigma");
+            expression = expression.Replace("β", @"\beta");
+            expression = expression.Replace("γ", @"\gamma");
+            expression = expression.Replace("η", @"\eta");
+            expression = expression.Replace("λ", @"\lambda");
+            expression = expression.Replace("ν", @"\nu");
+            expression = expression.Replace("ξ", @"\xi");
+            expression = expression.Replace("τ", @"\tau");
+            expression = expression.Replace("υ", @"\upsilon");
+            expression = expression.Replace("φ", @"\phi");
+            expression = expression.Replace("ψ", @"\psi");
+            expression = expression.Replace("ω", @"\omega");
+
+
             return expression;
         }
     }
