b0dd98ef7a68bc8935a2d7b1f2e34bba2d519281
Ben Samuels <bsamuels453@gmail.com>
2012-11-25 14:45:31 -0800

    setting an output field to readonly

--- Uncertainty-Propagation-Calculator/UncertaintyPropForm.Designer.cs
+++ Uncertainty-Propagation-Calculator/UncertaintyPropForm.Designer.cs
@@ -282,6 +282,7 @@
             this.FinalPropEquationField.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FinalPropEquationField.Location = new System.Drawing.Point(6, 159);
             this.FinalPropEquationField.Name = "FinalPropEquationField";
+            this.FinalPropEquationField.ReadOnly = true;
             this.FinalPropEquationField.Size = new System.Drawing.Size(366, 23);
             this.FinalPropEquationField.TabIndex = 16;
             // 
